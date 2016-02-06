3e4875a9b46bd102a03ca370fe6d1215a054bde3
Anthony Persaud <persaud@modernistik.com>
2016-02-06 11:29:46 -0800

    Added default SERVER_URL

--- lib/parse/client/protocol.rb
+++ lib/parse/client/protocol.rb
@@ -4,6 +4,7 @@ module Parse
 
   module Protocol
       HOST          = "api.parse.com".freeze
+      SERVER_URL    = "https://api.parse.com/1/".freeze
       APP_ID        = 'X-Parse-Application-Id'.freeze
       API_KEY       = 'X-Parse-REST-API-Key'.freeze
       MASTER_KEY    = 'X-Parse-Master-Key'.freeze
