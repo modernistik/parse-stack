973dedb78ebe03f023b7d0e19f75e6d358d29d96
Anthony Persaud <apersaud@gmail.com>
2016-09-19 16:52:40 -0700

    Experimental support for session actions.

--- lib/parse/api/users.rb
+++ lib/parse/api/users.rb
@@ -16,31 +16,31 @@ module Parse
       LOGIN_PATH = "login"
       PASSWORD_RESET = "requestPasswordReset"
 
-      def fetch_user(id)
-        request :get, "#{USER_PATH_PREFIX}/#{id}"
+      def fetch_user(id, headers: {}, **opts)
+        request :get, "#{USER_PATH_PREFIX}/#{id}", headers: headers, opts: opts
       end
 
-      def find_users(query = {})
-        response = request :get, "#{USER_PATH_PREFIX}", query: query
+      def find_users(query = {}, headers: {}, **opts)
+        response = request :get, "#{USER_PATH_PREFIX}", query: query, headers: headers, opts: opts
         response.parse_class = Parse::Model::CLASS_USER
         response
       end
 
-      def current_user(session_token)
-        headers = {Parse::Protocol::SESSION_TOKEN => session_token}
-        response = request :get, "#{USER_PATH_PREFIX}/me", headers: headers
+      def current_user(session_token, headers: {}, **opts)
+        headers.merge!({Parse::Protocol::SESSION_TOKEN => session_token})
+        response = request :get, "#{USER_PATH_PREFIX}/me", headers: headers, opts: opts
         response.parse_class = Parse::Model::CLASS_USER
         response
       end
 
-      def update_user(id, body = {})
-        response = request :put, "#{USER_PATH_PREFIX}/#{id}", body: body
+      def update_user(id, body = {}, headers: {}, **opts)
+        response = request :put, "#{USER_PATH_PREFIX}/#{id}", body: body, opts: opts
         response.parse_class = Parse::Model::CLASS_USER
         response
       end
 
-      def delete_user(id)
-        request :delete, "#{USER_PATH_PREFIX}/#{id}"
+      def delete_user(id, headers: {}, **opts)
+        request :delete, "#{USER_PATH_PREFIX}/#{id}", headers: headers, opts: opts
       end
 
       def reset_password(email, **opts)
@@ -49,19 +49,19 @@ module Parse
         request :post, PASSWORD_RESET, body: body, opts: opts
       end
 
-      def login(username, password, **opts)
+      def login(username, password, headers: {}, **opts)
         # Probably pass Installation-ID as header
         opts.merge!({use_master_key: false, cache: false})
         params = { USERNAME_PARAM => username, PASSWORD_PARAM => password }
-        headers = { Parse::Protocol::REVOCABLE_SESSION => '1'}
+        headers.merge!({ Parse::Protocol::REVOCABLE_SESSION => '1'})
         # headers.merge!( { Parse::Protocol::INSTALLATION_ID => ''} )
         response = request :get, LOGIN_PATH, query: params, headers: headers, opts: opts
         response.parse_class = Parse::Model::CLASS_USER
         response
       end
 
-      def logout(session_token, **opts)
-        headers = { Parse::Protocol::SESSION_TOKEN => session_token}
+      def logout(session_token, headers: {}, **opts)
+        headers.merge!({ Parse::Protocol::SESSION_TOKEN => session_token})
         opts.merge!({use_master_key: false, cache: false, session_token: session_token})
         request :post, LOGOUT_PATH, headers: headers, opts: opts
       end
