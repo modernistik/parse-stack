69bba862f946fd6ba7ba84367eac493d0e84acb6
Anthony Persaud <persaud@modernistik.com>
2018-05-07 13:02:07 -0700

    Fixes deprecation warning related to IndexDistance.

--- Modernistik/Core/Swift+Ext.swift
+++ Modernistik/Core/Swift+Ext.swift
@@ -75,7 +75,8 @@ extension MutableCollection {
         guard c > 1 else { return }
         
         for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
-            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            // Change `Int` in the next line to `IndexDistance` in < Swift 4.1
+            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
             let i = index(firstUnshuffled, offsetBy: d)
             swapAt(firstUnshuffled, i)
         }
