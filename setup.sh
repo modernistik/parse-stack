986977cf0acc68bd68f178781f7bece5f7f5da7a
Anthony Persaud <persaud@modernistik.com>
2019-07-27 16:06:36 -0700

    Adds new testable project for CI.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cd Examples/Starter; pod install; cd ../../
+cd Examples/Modernistik; pod install; cd ../../
 cd Examples/Slate; pod install; cd ../../
 cd Examples/Phoenix; pod install; cd ../../
 cd Examples/TV; pod install; cd ../../
