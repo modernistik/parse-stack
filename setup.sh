f0983f0b85ffac2a1bae725cdc1e99753090cc9c
Anthony Persaud <persaud@modernistik.com>
2019-07-27 16:38:11 -0700

    Removes Slate project.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 
 cd Examples/Starter; pod install; cd ../../
 cd Examples/Modernistik; pod install; cd ../../
-cd Examples/Slate; pod install; cd ../../
 cd Examples/Phoenix; pod install; cd ../../
 cd Examples/TV; pod install; cd ../../
