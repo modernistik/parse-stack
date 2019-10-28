bffa983e92bfa5dc7dd292d25860fb9eeb5b4325
Anthony Persaud <persaud@modernistik.com>
2019-10-27 19:36:00 -0700

    Add ModernPageController adopting ModernControllerConformance

--- Modernistik/UIKit+Ext.swift
+++ Modernistik/UIKit+Ext.swift
@@ -656,3 +656,27 @@ open class ModernCollectionController: ModernViewController, UICollectionViewDat
         return ModernCollectionCell.dequeueReusableCell(in: collectionView, for: indexPath)
     }
 }
+
+open class ModernPageController : UIPageViewController, ModernControllerConformance {
+    private var needsSetupConstraints = true
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.setNeedsUpdateConstraints()
+    }
+
+    open func setupConstraints() {}
+
+    open override func updateViewConstraints() {
+        if needsSetupConstraints {
+            needsSetupConstraints = false
+            setupConstraints()
+        }
+        // According to Apple's doc, it should be called as the final step in the
+        // implementation.
+        super.updateViewConstraints()
+    }
+
+    open func updateInterface() {}
+}
