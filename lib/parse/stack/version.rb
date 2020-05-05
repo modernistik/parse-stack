57b5a969653f072859145f6b74defe3c36e62d96
Anthony Persaud <persaud@modernistik.com>
2020-05-05 09:17:56 -0700

    Remove support for ActiveModel 6 or later.

--- lib/parse/stack/version.rb
+++ lib/parse/stack/version.rb
@@ -6,6 +6,6 @@ module Parse
   # The Parse Server SDK for Ruby
   module Stack
     # The current version.
-    VERSION = "1.8.0"
+    VERSION = "1.8.1"
   end
 end
--- test/lib/parse/version_test.rb
+++ test/lib/parse/version_test.rb
@@ -2,6 +2,6 @@ require_relative "../../test_helper"
 
 describe Parse::Stack do
   it "must be defined" do
-    Parse::Stack::VERSION.wont_be_nil
+    _(Parse::Stack::VERSION).wont_be_nil
   end
 end
